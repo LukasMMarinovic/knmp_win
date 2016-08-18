7d92398a8b5e09970eb5c9dc6f9112af43e41c75
Lukas Marinovic <kingofavocados53@gmail.com>
2016-08-17 21:37:32 -0500

    Update_1
    +plays meow noise

---
+++ KNMP_win/KNMP_win/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+
+///////////////////////////////////////////////
+//                                           //
+//    	            |\___|\                  //
+//           	   /       |.                //
+//   	          |  .     |_                //
+//    	          |          )               //
+//   	          |      -v-v                //
+//   	          /        |\                //
+//	          Scratch-A-Bear Media           //
+//-------------------------------------------//
+//--------------Lukas Marinovic--------------//
+//-------------------------------------------//
+// Please visit:                             //
+//  -Blog				                     //
+//	http://scratchabear.blogspot.com         //
+//  -Github                                  //
+//	https://github.com/LukasMMarinovic       //
+//                                           //
+/////////////////////////////////////////////// 
+
+
+namespace KNMP_win
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("//");
+
+            //Implement search for "meow.wav" file
+            var Player = new System.Media.SoundPlayer();
+            Player.SoundLocation = @"D:\Programs\knmp_win\meow.wav";
+            Player.PlaySync();
+        }
+    }
+}
